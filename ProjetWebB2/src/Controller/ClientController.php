eb75fe4dc8d5141718e5101a378aed4b80d1dfcb
Akane <soranyuxe@gmail.com>
2020-05-18 14:10:39 +0200

    panier

--- ProjetWebB2/src/Controller/ClientController.php
+++ ProjetWebB2/src/Controller/ClientController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Restaurant;
 use App\Repository\PlatRepository;
 use App\Repository\RestaurantRepository;
+use App\Service\Panier\PanierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 
 class ClientController extends AbstractController
 {
@@ -22,16 +24,55 @@ class ClientController extends AbstractController
     }
 
 
-        /**
+    /**
      * @Route("/client/{id}/restaurant", name="restaurant", methods={"GET"})
      */
-    public function restaurant(PlatRepository $platRepository, Restaurant $restaurant): Response
+    public function restaurant(PlatRepository $platRepository,
+     Restaurant $restaurant,
+     PanierService $panierService): Response
     {
+
+
+
+
         return $this->render('client/restaurant.html.twig', [
             'restaurant' => $restaurant,
             'plats' => $platRepository->findby([
                         'idRestaurant' => $restaurant->getId()
-        ])
+        ]),
+            "items" => $panierService->getAll(),
+            "total" => $panierService->getTotal()
             ]);
     }
+
+
+    /**
+     * @Route("/client/{id}/restaurant/ajouter/{idPlat}", name="panier_add")
+     * @Entity("plat", expr="repository.find(idPlat)")
+     */
+    public function add($idPlat, PanierService $panierService,Restaurant $restaurant)
+    {
+        $panierService->add($idPlat);
+
+        return $this->redirectToRoute("restaurant",[
+        'restaurant' => $restaurant,
+        'id' => $restaurant->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/client/{id}/restaurant//supprimer/{idPlat}", name="panier_remove")
+     * @Entity("plat", expr="repository.find(idPlat)")
+     */
+    public function remove($idPlat, PanierService $panierService,Restaurant $restaurant)
+    {
+        $panierService->remove($idPlat);
+
+        return $this->redirectToRoute('restaurant',[
+            'restaurant' => $restaurant,
+            'id' => $restaurant->getId()
+            ]);
+    }
+
+
 }
